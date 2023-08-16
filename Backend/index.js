ede7857ecba52c30e1534c2ed2bc1ed2618b253e
Ahmed Mehana <102672897+ahmed7mehana@users.noreply.github.com>
2023-08-17 02:49:40 +0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Cors Policy
 app.use(cors({
-  origin: 'http://sea-food-seven.vercel.app/',
+  origin: 'https://sea-food-seven.vercel.app/',
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
   credentials: true
 }));
